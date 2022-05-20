cda23c99423f37bcb454d8bf902a14febc1dbe9c
DavidAmyot <david.amyot@codeboxx.biz>
2022-05-20 17:56:53 -0400

    add server-side rendering

--- utils/user.js
+++ utils/user.js
@@ -2,12 +2,10 @@ export async function getRandomUserData() {
     const res = await fetch('https://randomuser.me/api/');
     const json = await res.json();
 
-    let object = {
+    return {
         name: `${json.results[0].name.first} ${json.results[0].name.last}`,
         email: json.results[0].email,
         phone: json.results[0].cell,
         gender: json.results[0].gender,
-    }
-
-    return object;
+    };
 }
